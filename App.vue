4ff85b09f92957eeb67de6691e1f6f4aeb4479fb
Metin <metin3232@yahoo.com>
2022-02-03 09:55:37 +0300

    Initial commit

---
+++ App.vue
@@ -0,0 +1,135 @@
+<template>
+  <div id="data-grid-demo">
+    <DxDataGrid
+      id="gridContainer"
+      :data-source="dataSource"
+      :show-borders="true"
+      :selected-row-keys="selectedItemKeys"
+      @selection-changed="selectionChanged"
+    >
+      <DxEditing
+        :allow-updating="true"
+        :allow-adding="true"
+        :allow-deleting="true"
+        mode="cell"
+      />
+      <DxPaging :enabled="false"/>
+      <DxSelection mode="multiple"/>
+      <DxColumn
+        :width="55"
+        data-field="Prefix"
+        caption="Title"
+      />
+      <DxColumn
+        data-field="FirstName"
+      />
+      <DxColumn
+        data-field="LastName"
+      />
+      <DxColumn
+        :width="170"
+        data-field="Position"
+      />
+      <DxColumn
+        :width="125"
+        data-field="StateID"
+        caption="State"
+      >
+        <DxLookup
+          :data-source="states"
+          value-expr="ID"
+          display-expr="Name"
+        />
+      </DxColumn>
+      <DxColumn
+        data-field="BirthDate"
+        data-type="date"
+      />
+      <DxToolbar>
+        <DxItem
+          name="addRowButton"
+          show-text="always"
+        />
+        <DxItem location="after">
+          <DxButton
+            @click="deleteRecords()"
+            :disabled="!selectedItemKeys.length"
+            icon="trash"
+            text="Delete Selected Records"
+          />
+        </DxItem>
+      </DxToolbar>
+    </DxDataGrid>
+  </div>
+</template>
+<script>
+import {
+  DxDataGrid,
+  DxColumn,
+  DxPaging,
+  DxEditing,
+  DxSelection,
+  DxLookup,
+  DxToolbar,
+  DxItem,
+} from 'devextreme-vue/data-grid';
+
+import { DxButton } from 'devextreme-vue/button';
+import DataSource from 'devextreme/data/data_source';
+import ArrayStore from 'devextreme/data/array_store';
+
+import { employees, states } from './data.js';
+
+export default {
+  components: {
+    DxDataGrid,
+    DxColumn,
+    DxPaging,
+    DxEditing,
+    DxSelection,
+    DxLookup,
+    DxButton,
+    DxToolbar,
+    DxItem,
+  },
+  data() {
+    return {
+      dataSource: new DataSource({
+        store: new ArrayStore({
+          data: employees,
+          key: 'ID',
+        }),
+      }),
+      selectedItemKeys: [],
+      states,
+      selectionChanged: (data) => {
+        this.selectedItemKeys = data.selectedRowKeys;
+      },
+      deleteRecords: () => {
+        this.selectedItemKeys.forEach((key) => {
+          this.dataSource.store().remove(key);
+        });
+        this.selectedItemKeys = [];
+        this.dataSource.reload();
+      },
+    };
+  },
+};
+</script>
+<style>
+#data-grid-demo {
+  min-height: 700px;
+}
+
+#gridDeleteSelected {
+  position: absolute;
+  z-index: 1;
+  right: 0;
+  margin: 1px;
+  top: 0;
+}
+
+#gridDeleteSelected .dx-button-text {
+  line-height: 0;
+}
+</style>
